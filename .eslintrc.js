072cd4e13f0cce78dfe03b94ff1f7c506eb79d09
Steve Cherneff <schernef@adobe.com>
2025-12-31 16:16:17 -0800

    Enhance audience handling and metadata retrieval in scripts.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,10 @@
 module.exports = {
   root: true,
-<<<<<<< HEAD
   extends: [
     'airbnb-base',
     'plugin:json/recommended',
     'plugin:xwalk/recommended',
   ],
-=======
-  extends: 'airbnb-base',
->>>>>>> 698582f (Squashed 'plugins/experimentation/' content from commit 0356a22)
   env: {
     browser: true,
   },
@@ -19,17 +15,8 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
-<<<<<<< HEAD
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
-=======
-    // allow reassigning param
-    'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
-    'import/extensions': ['error', {
-      js: 'always',
-    }],
->>>>>>> 698582f (Squashed 'plugins/experimentation/' content from commit 0356a22)
   },
 };
